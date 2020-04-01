f3fa177493727edff05b3aa4423e8139150fdaba
Ian Rosen <irosen419@gmail.com>
2020-04-01 14:18:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,19 +7,16 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction("Ian")
 
 def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Ian", "Ruby")
 
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional("Ian")
 
 
 
